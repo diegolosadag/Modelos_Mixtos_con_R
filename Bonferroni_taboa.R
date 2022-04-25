9b7cfbd58e183395d0fa2a2e21e18cfa6ce572d7
diegolosadag <83222409+diegolosadag@users.noreply.github.com>
2022-04-25 10:15:47 +0200

    Add files via upload

---
+++ Bonferroni_taboa.R
@@ -0,0 +1,104 @@
+
+######################################################
+#####   INTERVALOS DE CONFIANZA SIMULTANEOS
+#####        POLO METODO DE BONFERRONI
+##### E BUSQUEDA AUTOMATICA DOS GRUPOS QUE DIFIREN
+##
+###                   Diego Losada Gonzalez
+######################################################
+
+######################################################
+#####   SIMULTANEOUS CONFIDENCE INTERVALS
+#####        BY THE BONFERRONI METHOD
+##### AND AUTOMATIC SEARCH OF GROUPS THAT DIFFER
+##
+###                   Diego Losada Gonzalez
+######################################################
+
+
+Bonferroni_taboa <- function(varresposta, varfactor, alpha_sen_CB = .05, ndixitos = NULL){
+  
+  if (!is.numeric(varresposta)){
+    stop("varresposta debe ser da clase numeric, proba con as.numeric(varresposta).")
+  }
+  if (!is.factor(varfactor)){
+    stop("varfactor debe ser da clase factor, proba con factor(varfactor).")
+  }
+  if (!is.numeric(alpha_sen_CB)){
+    stop("O nivel de significacion sen correccion de Bonferroni alpha_sen_CB ten que ser un numero.")
+  }
+  if (!is.numeric(ndixitos) & !is.null(ndixitos) | ndixitos < 0){
+    stop("O número de díxitos ndixitos ten que ser un número natural.")
+  }
+  
+  n = length(varresposta)
+  J = length(levels(varfactor))
+  nj = table(varfactor)
+  desvtip = sqrt(deviance(lm(varresposta ~ varfactor))/(n-J))
+  comb <- gtools::combinations(J, 2, 1:J) #ncomp = J*(J-1)/2
+  alpha_con_CB <- alpha_sen_CB / (2 * nrow(comb))
+  ct = qt(1 - alpha_con_CB, n - J)
+  Bonferroni = data.frame(dif = rep(0, nrow(comb)), inf = rep(0, nrow(comb)), sup = rep(0, nrow(comb)))
+  for (k in 1:nrow(comb)){
+    rownames(Bonferroni)[k] <- paste(comb[k, 1], comb[k, 2], sep = "-")
+  }
+  mu_local <- numeric(7)
+  for (i in 1:length(levels(varfactor))){
+    mu_local[i] <- mean(varresposta[varfactor == levels(varfactor)[i]])
+  }
+  for (k in 1:nrow(comb)){
+    Bonferroni$dif[k] = mu_local[comb[k, 2]] - mu_local[comb[k, 1]]
+    Bonferroni$inf[k] = mu_local[comb[k, 2]] - mu_local[comb[k, 1]] - ct * desvtip * sqrt(1/nj[comb[k, 2]] + 1/nj[comb[k, 1]])
+    Bonferroni$sup[k] = mu_local[comb[k, 2]] - mu_local[comb[k, 1]] + ct * desvtip * sqrt(1/nj[comb[k, 2]] + 1/nj[comb[k, 1]])
+  }
+  cat("#####  Intervalos de confianza simultáneos", "\n", " # con correccion de Bonferroni:  #####", "\n")
+  if (!is.null(ndixitos)){
+    print(round(Bonferroni, ndixitos))
+  }
+  else{ print(Bonferroni)}
+  cat("\n", "#####  Grupos cuxas medias difiren:  #####")
+  difiren <- numeric(nrow(comb))
+  nondifiren <- numeric(nrow(comb))
+  for (k in 1:nrow(comb)){
+    if(sign(Bonferroni[k, 2]) == sign(Bonferroni[k, 3])){
+      cat("\n", "- Os grupos", rownames(Bonferroni)[k], "difiren nas súas medias")
+      difiren[k] <- rownames(Bonferroni)[k]
+    }
+    else{
+      nondifiren[k] <- rownames(Bonferroni)[k]
+    }
+  }
+  difiren_ch <- difiren[nondifiren == 0]
+  nondifiren_ch <- nondifiren[difiren == 0]
+  
+  saida <- list("intervsim" = Bonferroni, "difiren" = difiren_ch, "nondifiren" = nondifiren_ch, "alpha_CB" = alpha_con_CB)
+  return(invisible(saida))
+}
+
+### Para cargala no teu script, ubicala en directorio e cargar source("Bonferroni_taboa.R").
+#Os numeros dos grupos correspondense a orde na que se atopan no vector levels(varfactor), e.g., 
+  # 1 <- "setosa", 2 <- "versicolor", etc.
+
+### EXEMPLOS DE USO
+# iris_bonf <- Bonferroni_taboa(iris$Sepal.Length, iris$Species, alpha_sen_CB = 0.05, ndixitos = 2)
+# levels(iris$Species) #orde: 1 <- "setosa", 2 <- "versicolor", etc.
+# names(iris_bonf)
+# iris_bonf$intervsim #de clase data.frame, intervalos de confianza simultaneos
+# iris_bonf$difiren #grupos que difiren
+# iris_bonf$nondifiren #grupos que non difiren
+# iris_bonf$alpha_CB #nivel de significacion que se esta empregando coa correccion de Bonferroni
+
+#-------------------------------------------------- English instructions
+### To upload it to your script, put it in your directory and load it with source("Bonferroni_taboa.R").
+#The numbers of groups are those corresponding to the order in which they are in the vector levels(varfactor), e.g., 
+# 1 <- "setosa", 2 <- "versicolor", etc.
+
+### USE EXAMPLES
+# iris_bonf <- Bonferroni_taboa(iris$Sepal.Length, iris$Species, alpha_sen_CB = 0.05, ndixitos = 2)
+# levels(iris$Species) #order: 1 <- "setosa", 2 <- "versicolor", etc.
+# names(iris_bonf)
+# iris_bonf$intervsim # data.frame class, simultaneous confidence intervals
+# iris_bonf$difiren #groups that differ
+# iris_bonf$nondifiren #groups that does not differ
+# iris_bonf$alpha_CB #significance level being used with the correction of Bonferroni
+
---
+++ mates7_clasica.R
@@ -0,0 +1,211 @@
+
+######################################################
+#####   
+#####        SCRIPT: CODIGO TFG ESTATISTICA CLASICA
+##### INTRODUCION AOS MODELOS MIXTOS
+##         INTRODUCION E CAPITULO 1
+###                   Diego Losada Gonzalez
+######################################################
+
+rm(list = ls())
+library(nlme); library(ggplot2); library(lme4); library(car)
+library(dplyr); library(RColorBrewer); library(viridis)
+library(gtools); library(patchwork); library(knitr)
+library(lmtest); library(ggthemes); library(lattice)
+library(modeest); library(latex2exp)
+
+######################################
+##
+##   Creacion da base de datos mates
+##
+######################################
+
+mates <- merge(MathAchieve, MathAchSchool, by = "School")
+mates <- mates[, -12]
+names(mates) <- c("Escola", "SocialMin", "Sexo", "StSE", "NotaMates", "MediaStSE", "Tamaño", "Sector", 
+                  "Particip", "AmbDiscrim", "MaioriaMin")
+levels(mates$Sector) <- c("Publica", "Catolica")
+levels(mates$SocialMin) <- c("Non", "Si")
+levels(mates$Sexo) <- c("Home", "Muller")
+mates$Escola <- factor(mates$Escola, levels = sort(levels(mates$Escola))) #escolas ben ordeadas
+levels(mates$Escola) <- c(1:160) #renomeanse
+attach(mates)
+
+
+######################################
+##
+##   Introducion
+##
+######################################
+
+set.seed(123)
+ind <- sample(1:dim(mates)[1], 40) #40 alumnas/os aleatorios
+mls0 <- lm(NotaMates[ind] ~ StSE[ind])
+eps <- residuals(mls0)
+
+ggplot(data.frame(cbind(StSE[ind], NotaMates[ind])), aes(x = StSE[ind], y = NotaMates[ind])) +
+  geom_point() +
+  xlab("Status socio-económico de 40 nenas/os") + ylab("Nota en Matemáticas de 40 nenas/os") +
+  geom_smooth(method='lm', se = FALSE, col = "darkcyan") +
+  geom_segment(x = StSE[ind][12], y = NotaMates[ind][12], xend = StSE[ind][12], 
+               yend = NotaMates[ind][12] - eps[12], color = "tomato3", arrow = arrow(), size = 1) +
+  geom_point( aes(x = StSE[ind][12], y = NotaMates[ind][12] ), col = "purple4", size = 4 ) +
+  annotate("text", x = StSE[ind][12] + 1/6, y = NotaMates[ind][12] - eps[12]/2 + 1/3, 	parse = TRUE,
+           label = expression(widehat(epsilon)[i]), size = 10, col = "tomato3", cex = 2) +
+  annotate("text", x = StSE[ind][12] - 1/6, y = NotaMates[ind][12], 	parse = TRUE,
+           label = expression(Y[i]), size = 9, col = "purple4", cex = 2) +
+  annotate("text", x = 0.1, y = mls0$coefficients[[1]] - 2, parse = TRUE, label =
+             expression(paste(widehat(beta)[0], + widehat(beta)[1], "x")), size = 8, col = "darkcyan") +
+  #	theme_bw() +
+  theme(
+    axis.title.x = element_text(size = 20),
+    axis.text.x = element_text(size = 18),
+    axis.title.y = element_text(size = 20),
+    axis.text.y = element_text(size = 18))
+
+
+######################################
+##
+##   Creacion da base de datos mates7
+##
+######################################
+
+escolas_particip100 <- which(MathAchSchool$PRACAD == 1) #escolas con participacion unanime
+alum_escolas_particip100 <- which(Escola %in% escolas_particip100) #alumnos de escolas con participacion unanime
+length(alum_escolas_particip100) #307 alumnos de 7 escolas
+
+mates7 <- mates[alum_escolas_particip100, ] #evitamos sesgos de eleccion
+dim(mates7) # 307 alumnas/os e 11 variables
+mates7$Escola <- factor(mates7$Escola, levels = escolas_particip100) #corrixir levels
+levels(mates7$Escola) <- paste("E", 1:7, sep = "") #Cambiase o nome destas 7 escolas
+attach(mates7)
+
+
+######################################
+##
+##   ANOVA
+##
+######################################
+
+anova_mates7 <- lm(NotaMates ~ Escola - 1)
+summary(anova_mates7)
+mu_local <- numeric(7)
+for (i in 1:length(levels(mates7$Escola))){
+  mu_local[i] <- mean(NotaMates[Escola == levels(mates7$Escola)[i]])
+} #vector de medias locais
+
+
+ggplot(mates7, aes(x = Escola, y = NotaMates, color = Escola)) + 
+  geom_boxplot() +
+  labs(x = "Escolas", y = "Notas en Matemáticas", color = "Escolas") +
+  scale_color_brewer(palette = "Dark2") +
+  geom_segment(x = 0.625, y = mu_local[1], xend = 	1.375, yend = mu_local[1], linetype = "dotted", col = "firebrick4") +
+  geom_segment(x = 1.625, y = mu_local[2], xend = 	2.375, yend = mu_local[2], linetype = "dotted", col = "firebrick4") +
+  geom_segment(x = 2.625, y = mu_local[3], xend = 	3.375, yend = mu_local[3], linetype = "dotted", col = "firebrick4") +
+  geom_segment(x = 3.625, y = mu_local[4], xend = 	4.375, yend = mu_local[4], linetype = "dotted", col = "firebrick4") +
+  geom_segment(x = 4.625, y = mu_local[5], xend = 	5.375, yend = mu_local[5], linetype = "dotted", col = "firebrick4") +
+  geom_segment(x = 5.625, y = mu_local[6], xend = 	6.375, yend = mu_local[6], linetype = "dotted", col = "firebrick4") +
+  geom_segment(x = 6.625, y = mu_local[7], xend = 	7.375, yend = mu_local[7], linetype = "dotted", col = "firebrick4") +
+  geom_hline(yintercept = mean(NotaMates), linetype = "dashed", col = "navy") +
+  #theme_bw() +
+  theme(
+    legend.position = "none",
+    axis.title.x = element_text(size = 18),
+    axis.text.x = element_text(size = 16),
+    axis.title.y = element_text(size = 18),
+    axis.text.y = element_text(size = 16))
+
+
+#----------------------------------------------------#
+#Test F e contrastes pareados
+anova(lm(NotaMates ~ Escola)) #Taboa ANOVA
+pf(10.475, df1 = 6, df2 = 300, lower.tail = F) #p-valor
+source("Bonferroni_taboa.R") #cargado da funcion, vexase Bonferroni_taboa.R (GitHub)
+Bonferroni_taboa(NotaMates, Escola, alpha_sen_CB = 0.001, ndixitos = 3)
+mates7_bonf <- Bonferroni_taboa(NotaMates, Escola, alpha_sen_CB = 0.001, ndixitos = 3) 
+mates7_bonf$intervsim # intervalos de confianza simultaneos
+mates7_bonf$difiren # difiren as escolas: E1-E4, E3-E4, E3-E7 e E4-E5
+mates7_bonf$nondifiren # escolas que non difiren
+mates7_bonf$alpha_CB #alpha empregado coa corrección de Bonferroni
+
+
+######################################
+##
+##   ANCOVA
+##
+######################################
+
+ancova1 <- lm(NotaMates ~ StSE + Escola) #ANCOVA sen interaccion
+ancova2 <- lm(NotaMates ~ StSE * Escola) #ANCOVA con interaccion
+
+anova(ancova1, ancova2) #test F, interacción non significativa
+
+#Contraste sobre o efecto dos grupos e de StSE
+mod_StSE <- lm(NotaMates ~ StSE) #sen grupos
+mod_g <- lm(NotaMates ~ Escola) #sen StSE
+anova(ancova1, mod_StSE) #efecto das escolas significativo
+anova(ancova1, mod_g) #efecto do StSE significativo
+
+###-------------------------------GRAFICO ANCOVA------------
+
+coefs_anc1 <- ancova1$coefficients #estimacions ANCOVA sen interacción
+display.brewer.pal(n = 7, name = "Dark2") #Paleta de cores que se vai empregar
+dark_2 <- brewer.pal(n = 7, name = "Dark2")
+rang <- data.frame(i = numeric(7), f = numeric(7))
+for (k in 1:length(levels(Escola))){
+  rang[k ,] <- range(StSE[Escola == levels(Escola)[k]])
+} #Funcion para os rangos das rectas
+recta <- function(x = 0, grupo = 1){
+  if (!grupo %in% 1:7){
+    stop("Tal grupo non existe.")
+  }
+  if (grupo == 1){
+    as.numeric(coefs_anc1[1]) + as.numeric(coefs_anc1[2]) * x
+  }
+  else{
+    as.numeric(coefs_anc1[1]) + as.numeric(coefs_anc1[grupo + 1]) + as.numeric(coefs_anc1[2]) * x
+  }
+} #Funcion para as ecuacions das rectas
+
+ancova_si <- ggplot(mates7, aes(x = StSE, y = NotaMates, color = Escola)) + 
+  geom_point() +
+  labs(x = "Status socio-económico", y = "Notas en Matemáticas", color = "Escolas") +
+  coord_cartesian(ylim = c(-.5, 25) ) +
+  scale_color_brewer(palette = "Dark2") +
+  geom_segment(x = rang[1,1], xend = rang[1,2], y = recta(rang[1,1]), yend = recta(rang[1,2]), 
+               col = dark_2[1], lwd = 1.25) +
+  geom_segment(x = rang[2,1], xend = rang[2,2], y = recta(rang[2,1], 2), yend = recta(rang[2,2], 2), 
+               col = dark_2[2], lwd = 1.25) +
+  geom_segment(x = rang[3,1], xend = rang[3,2], y = recta(rang[3,1], 3), yend = recta(rang[3,2], 3), 
+               col = dark_2[3], lwd = 1.25) +
+  geom_segment(x = rang[4,1], xend = rang[4,2], y = recta(rang[4,1], 4), yend = recta(rang[4,2], 4), 
+               col = dark_2[4], lwd = 1.25) +
+  geom_segment(x = rang[5,1], xend = rang[5,2], y = recta(rang[5,1], 5), yend = recta(rang[5,2], 5), 
+               col = dark_2[5], lwd = 1.25) +
+  geom_segment(x = rang[6,1], xend = rang[6,2], y = recta(rang[6,1], 6), yend = recta(rang[6,2], 6), 
+               col = dark_2[6], lwd = 1.25) +
+  geom_segment(x = rang[7,1], xend = rang[7,2], y = recta(rang[7,1], 7), yend = recta(rang[7,2], 7), 
+               col = dark_2[7], lwd = 1.25) +
+  theme(
+    legend.position = "none",
+    axis.title.x = element_text(size = 18),
+    axis.text.x = element_text(size = 16),
+    axis.title.y = element_text(size = 18),
+    axis.text.y = element_text(size = 16))
+
+ancova_ci <- ggplot(mates7, aes(x = StSE, y = NotaMates, color = Escola)) + 
+  geom_point() +
+  labs(x = "Status socio-económico", color = "Escolas") +
+  scale_color_brewer(palette = "Dark2") +
+  geom_smooth(method = "lm", se = FALSE, lwd = 1.25) +
+  #  theme_bw() +
+  theme(
+    legend.title = element_text(size = 18),
+    legend.text = element_text(size = 16),
+    axis.title.x = element_text(size = 18),
+    axis.text.x = element_text(size = 16),
+    axis.title.y = element_blank(),
+    axis.text.y = element_text(size = 16))
+
+ancova_si + ancova_ci
+
